6d4f9f329979489bf4bca1c74019daa3ada3331a
Tal Ben-Nun <talbn@cs.huji.ac.il>
2016-01-27 19:49:54 +0200

    Removed clang 3.6

--- travis/install.sh
+++ travis/install.sh
@@ -12,7 +12,7 @@ apt-get -y update
 apt-get install \
     wget git curl \
     libgflags-dev libgoogle-glog-dev \
-    bc gcc-4.9 clang-3.6
+    bc gcc-4.9
 
 # gtest
 wget --no-check-certificate https://github.com/google/googletest/archive/release-1.7.0.zip -O gtest.zip

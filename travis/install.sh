6f60273d65d0ec68a3c80642d67dd58d4da4de09
Tal Ben-Nun <talbn@cs.huji.ac.il>
2016-01-28 14:35:18 +0200

    once more

--- travis/install.sh
+++ travis/install.sh
@@ -5,8 +5,9 @@
 
 set -e
 
-# Set GCC 4.9 and Clang 3.6 to be the defaults
-apt-get remove gcc-4.6 clang
+# Set GCC 4.9 and Clang 3.6 to be the defaults (1)
+update-alternatives --remove-all gcc
+update-alternatives --remove-all g++
 
 # This ppa is for gflags and glog
 add-apt-repository -y ppa:tuleu/precise-backports
@@ -17,6 +18,18 @@ apt-get install \
     wget git curl \
     libgflags-dev libgoogle-glog-dev \
     bc gcc-4.9 clang
+
+# Set GCC 4.9 and Clang 3.6 to be the defaults (2)
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 10
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 20
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.6 10
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 20
+update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 30
+update-alternatives --set cc /usr/bin/gcc
+update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 30
+update-alternatives --set c++ /usr/bin/g++
+update-alternatives --set gcc /usr/bin/gcc-4.9
+update-alternatives --set g++ /usr/bin/g++-4.9
 
 
 # gtest

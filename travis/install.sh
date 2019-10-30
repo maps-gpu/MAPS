ee89349dccbcd52f4192c07df76d44f870e92c78
Tal Ben-Nun <tbennun@users.noreply.github.com>
2019-10-30 21:55:12 +0100

    Update install.sh

--- travis/install.sh
+++ travis/install.sh
@@ -13,7 +13,7 @@ chmod -R a+rw win/gtest
 rm -rf googletest-release-1.7.0 gtest.zip
 
 # CMake
-wget --no-check-certificate http://www.cmake.org/files/v3.2/cmake-3.2.3-Linux-x86_64.sh -O cmake3.sh
+wget --no-check-certificate https://github.com/Kitware/CMake/releases/download/v3.15.5/cmake-3.15.5-Linux-x86_64.sh -O cmake3.sh
 chmod +x cmake3.sh
 ./cmake3.sh --prefix=/usr/ --skip-license --exclude-subdir
 rm -f ./cmake3.sh

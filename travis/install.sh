63389d8c9473bd36c44814d2282fb5259ebfe8a5
Tal Ben-Nun <tbennun@users.noreply.github.com>
2019-10-30 17:57:33 +0100

    Update install.sh

--- travis/install.sh
+++ travis/install.sh
@@ -3,16 +3,6 @@
 
 set -e
 
-# Set GCC 4.9 as the default compiler
-#update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 99
-#update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 99
-#update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 30
-#update-alternatives --set cc /usr/bin/gcc
-#update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 30
-#update-alternatives --set c++ /usr/bin/g++
-#update-alternatives --set gcc /usr/bin/gcc-4.9
-#update-alternatives --set g++ /usr/bin/g++-4.9
-
 # gtest
 wget --no-check-certificate https://github.com/google/googletest/archive/release-1.7.0.zip -O gtest.zip
 unzip gtest.zip
